b44224f4148e5f262b8256fd75b05df33159942b
JackMarx <seriousfools@gmail.com>
2016-05-11 18:30:46 -0500

    forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/one' => 'contacts#one'
   get '/many' => 'contacts#many'
+  get '/new_form' => 'contacts#new_contact_form'
+  post '/make_new_contact' => 'contacts#make_new_contact'
 end
