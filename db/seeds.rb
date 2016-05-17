31d08040613d289a6d02af1a4a3f5b6b531eaa0a
JackMarx <seriousfools@gmail.com>
2016-05-17 08:05:16 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Contact.destroy_all
+
+Contact.create(
+  [
+    {
+      first_name: "Mickey",
+      last_name: "Mouse",
+      phone_number: "310-555-1234",
+      email: "bigears@gmail.com"
+    },
+    {
+      first_name: "Minnie",
+      last_name: "Mouse",
+      phone_number: "310-555-1235",
+      email: "bowonear@gmail.com"
+    },
+    {
+      first_name: "Goofy",
+      last_name: "Goof",
+      phone_number: "310-555-9876",
+      email: "bigfeet@gmail.com"
+    },
+    {
+      first_name: "Donald",
+      last_name: "Duck",
+      phone_number: "310-555-5678",
+      email: "nopants@gmail.com"
+    },
+    {
+      first_name: "Daisy",
+      last_name: "Duck",
+      phone_number: "310-555-6543",
+      email: "webbedstylishtoes@gmail.com"
+    },
+  ]
+  )
